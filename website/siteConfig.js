8b2e56daa236cf734303d53a33ed0d542e85aa35
Dmstanto <71608009+Dmstanto@users.noreply.github.com>
2021-10-31 22:30:55 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: 'tonya-melvin-bryant-ab92b855',
-        twitter: 'tonyamccu',
-        github: 'tamelvinbryant',
+        linkedin: 'daniel-stanton-814237151',
+        twitter: 'N/A',
+        github: 'dmstanto',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
